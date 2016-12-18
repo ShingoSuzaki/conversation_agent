677b6fe36f43cac63cc26505cb16c30140e84ce1
Shingo <eclair.1008@gmail.com>
2016-12-18 23:33:44 +0900

    modify test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import PyQt5 as pq5
-if __name__ == '__main__'
-    app = pq5.QtWidgets.QApplication(sys.argv)
+from PyQt5 import QtWidgets 
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
 
 
     window = QtWidgets.QWidget()
